9bc420ab474b27962fa8bd003e821e5002dc1475
root <vainqueurmeek@gmail.com>
2023-06-18 16:01:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  *main - point
-  *return: always 0 (success)
-  */
+ * main - Entry point of the program
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
