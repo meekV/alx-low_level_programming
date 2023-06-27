d483c332242d247420bf549967b19dc4e2f07608
meekV <vainqueurmeek@gmail.com>
2023-06-26 20:43:43 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The string to print the second half of
+ */
+void puts_half(char *str)
+{
+    int length = 0;
+    int i, start_index;
+
+    /* Calculate the length of the string */
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    /* Determine the starting index based on string length */
+    if (length % 2 == 0)
+    {
+        start_index = length / 2;
+    }
+    else
+    {
+        start_index = (length + 1) / 2;
+    }
+
+    /* Print the second half of the string */
+    for (i = start_index; i < length; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
