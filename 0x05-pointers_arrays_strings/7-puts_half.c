fe7aab73ff52af0cf6e1b3009b1707e09b52cccd
meekV <vainqueurmeek@gmail.com>
2023-06-27 06:58:07 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,29 +6,29 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i, start_index;
+	int length = 0;
+	int i, start_index;
 
-    /* Calculate the length of the string */
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Determine the starting index based on string length */
-    if (length % 2 == 0)
-    {
-        start_index = length / 2;
-    }
-    else
-    {
-        start_index = (length + 1) / 2;
-    }
+	/* Determine the starting index based on string length */
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length + 1) / 2;
+	}
 
-    /* Print the second half of the string */
-    for (i = start_index; i < length; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	/* Print the second half of the string */
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
