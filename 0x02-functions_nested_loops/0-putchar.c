eb823401702930865f246f5fffe15550def66483
meekV <vainqueurmeek@gmail.com>
2023-08-15 10:37:04 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,19 @@
 /**
  * main - Entry point
  * Description: 'prints _putchar to stdout'
- * Return: always 0 
+ * Return: always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+ 	_putchar('_');
+ 	_putchar('p');
+ 	_putchar('u');
+ 	_putchar('t');
+ 	_putchar('c');
+ 	_putchar('h');
+ 	_putchar('a');
+ 	_putchar('r');
+ 	_putchar('\n');
+ 	return (0);
 }
+
